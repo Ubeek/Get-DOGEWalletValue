89aa6199039f0d7957eb907f34a783d90efbd470
Ubeek <git@ubeek.net>
2014-01-05 14:05:21 +0800

    Added support for multiple wallets

--- Get-DOGEWalletValue.ps1
+++ Get-DOGEWalletValue.ps1
@@ -1,18 +1,26 @@
 Param([string]$wallet)
-If(!$wallet){$wallet = Read-Host "Please enter DOGE wallet address"}
+If(!$wallet){$wallet = Read-Host "Please enter DOGE wallet address (Seperate with comma if multiple)"}
 
 $walletAPI = "http://dogechain.info/chain/CHAIN/q/addressbalance"
 $cryptoAPI = "http://www.cryptocoincharts.info/v2/api/tradingPair"
 $pairDOGEtoBTC = "DOGE_BTC"
 $pairBTCtoUSD = "BTC_USD"
+$balance = 0
 
 $proxy = [System.Net.WebRequest]::GetSystemWebProxy()
 $proxy.Credentials = [System.Net.CredentialCache]::DefaultCredentials
 $webclient = New-Object Net.Webclient
 $webclient.proxy = $proxy
 
-#Get num of DOGE in wallet
-$balance = $webclient.DownloadString("$walletAPI/$wallet")
+#Get num of DOGE in wallet(s)
+$things = $wallet.Split(",")
+Write-Host "Processing $($things.count) wallet(s)"
+Foreach($w in $things)
+{
+    $wbalance = $webclient.DownloadString("$walletAPI/$w")
+    $balance = [double]$balance + [double]$wbalance
+}
+
 
 #Get current DOGE to BTC price
 #DOGEtoBTC contains following properties: ID,price,price_before_24,volume_first,volume_second,volume_btc,best_market,latest_trade
@@ -26,9 +34,18 @@ $BTCtoUSD = $strBTCtoUSD | ConvertFrom-JSON
 
 $BTCValue = [double]$balance * [double]$DOGEtoBTC.price
 $USDvalue = [double]$BTCValue * [double]$BTCtoUSD.price
-Write-Host "DOGECoin Wallet Address: $wallet"
+$result = @{}
+$result.Add("DOGE Wallet(s)", $wallet)
+$result.Add("Wallet Balance",$balance)
+$result.Add("Balance in BTC",$BTCValue)
+$result.Add("Best market(DOGE to BTC)",$DOGEtoBTC.best_market)
+$result.Add("Balance in USD",$USDValue)
+$result.Add("Best market(BTC to USD)",$BTCtoUSD.best_market)
+Write-Host "DOGECoin Wallet(s): $wallet"
 Write-Host "Balance of Wallet: $balance"
 Write-Host "Best Market Currently (DOGE\BTC): $($DOGEtoBTC.best_market)"
 Write-Host "Balance of wallet in BTC: $BTCValue"
 Write-Host "Best Market Currently (BTC\USD): $($BTCtoUSD.best_market)"
 Write-Host "Balance of wallet in USD: $USDValue"
+#Write-Host "`n--------------`n"
+#$result | ft
